99178afd3bc324ede1b0eea1aa285ce1ecc6f759
Lander <lander@vanderstighelen.net>
2024-05-05 17:20:42 +0000

    Refactor fetch module and update main flow

--- exercise/main.py
+++ exercise/main.py
@@ -1,12 +1,12 @@
 from prefect import flow
-from fetch import kaggle_dataset_downloader
+from fetch.fetch import dataset_download_flow
 
 DATASET_NAME = "likhon148/animal-data"
 DATASET_PATH = "../data"
 
 @flow
 def main_flow():
-    kaggle_dataset_downloader(DATASET_NAME, DATASET_PATH)
+    dataset_download_flow(DATASET_NAME, DATASET_PATH)
 
 
 if __name__ == "__main__":
